35ca8f19215c00539bacd98defd686ffe1ed567f
Rohan <kolwalerohan8@gmail.com>
2025-09-29 23:28:11 +0530

    added new file

---
+++ lab.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class lab {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.print("Prime");
+        } else {
+            System.out.print("NOT Prime");
+        }
+    }
+}
