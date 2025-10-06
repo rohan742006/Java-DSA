7d4f102586d69b96f9b89fa0e6694a4c9231d5c2
Rohan <kolwalerohan8@gmail.com>
2025-10-06 15:46:40 +0530

    Some new functions are added

--- stack.java
+++ stack.java
@@ -58,11 +58,56 @@ public class stack{
     }
         return false;  //no duplicate found
     }
+
+    public static void max_area_histo(int array[]){
+        int nsl[]=new int[array.length];  //store next small left
+        int nsr[]=new int[array.length];   //store next small right
+        //nsl
+        Stack<Integer>s=new Stack<>();
+        for(int i=0;i<array.length;i++){
+            while(!s.isEmpty() && array[s.peek()]>=array[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i]=-1;
+            }
+            else{
+                nsl[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+        //nsr
+        s.clear();
+        for(int i=array.length-1;i>=0;i--){
+            while(!s.isEmpty() && array[s.peek()]>=array[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i]=array.length;
+            }
+            else{
+                nsr[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+        int max=0;
+        for(int i=0;i<array.length;i++){
+            int h=array[i];
+            int w=nsr[i]-nsl[i]-1;
+            int area=h*w;
+            max=Math.max(area,max);
+        }
+        System.out.print("The max area in histogram is: "+max);
+    }
     public static void main(String args[]){
         //String str="({})[]";
         //System.out.print(valid_parenthesis(str));
-        String str="(a+b)";
-        System.out.print(duplicate_parentheses(str));
+        //String str="(a+b)";
+        //System.out.print(duplicate_parentheses(str));
+        int heights[]={2,4};
+        max_area_histo(heights);
     }
 }
 
