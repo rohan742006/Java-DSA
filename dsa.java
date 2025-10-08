9a5e419c529209055051c00a97bf16e81b0a6fa0
Rohan <kolwalerohan8@gmail.com>
2025-10-08 16:16:22 +0530

    new file added

---
+++ dsa.java
@@ -0,0 +1,77 @@
+import java.util.*;
+public class dsa {
+
+    static class Node{
+        char data;
+        Node next;
+
+        public Node(char data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+        public static Node head;
+        public static Node tail;
+        
+        public static void addlast(char data){
+            Node newNode=new Node(data);
+            if(head==null){
+                head=tail=newNode;
+                return;
+            }
+            tail.next=newNode;
+            tail=newNode;
+        }
+
+    public static boolean palindrome(Node head){
+        if(head==null || head.next==null){
+            return true;
+        }
+        Node curr=mid(head);
+        Node prev=null;
+        Node next;
+
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        //compare bot halves
+        Node left=head;
+        Node right=prev;
+
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+
+    public static Node mid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.next;
+    }
+    public static void main(String args[]){
+        addlast('a');
+        addlast('b');
+        addlast('b');
+        addlast('a');
+        if(palindrome(head)){
+            System.out.print("List is Palindrome");
+        }
+        else{
+            System.out.print("List is not Plaindrome");
+        }
+    }
+}
